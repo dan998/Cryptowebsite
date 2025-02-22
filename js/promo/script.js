d22fef2fd54be861b9ffba795e95ff7be2588bba
Dan Maffia <73266204+dan998@users.noreply.github.com>
2025-02-22 00:30:29 -0400

    Add files via upload

--- js/promo/script.js
+++ js/promo/script.js
@@ -1,1 +1,9 @@
-
+// Function to toggle the visibility of the dropdown content
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none"; // Hide the dropdown if it's already visible
+    } else {
+        dropdown.style.display = "block"; // Show the dropdown
+    }
+}
